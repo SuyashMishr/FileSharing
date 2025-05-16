bb7684165ea02dc828c4ec20b617c369d2474a07
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-05-16 18:36:04 +0530

    Update image-controller.js

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -1,35 +1,29 @@
 import File from '../models/file.js';
-import bcrypt from 'bcrypt';
-import dotenv from 'dotenv';
 
-dotenv.config();
+export const uploadImage = async (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+  const fileObj = {
+    path: req.file.path,
+    name: req.file.originalname
+  };
 
-export const uploadImage = async (request, response) => {
-    const fileObj = {
-        path: request.file.path,
-        name: request.file.originalname,
-    }
-    
-    try {
-        const file = await File.create(fileObj);
-        response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}`});
-    } catch (error) {
-        console.error(error.message);
-        response.status(500).json({ error: error.message });
-    }
-}
+  try {
+    const file = await File.create(fileObj);
+    res.status(200).json({ path: `http://localhost:8000/file/${file._id}` });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-export const getImage = async (request, response) => {
-    try {   
-        const file = await File.findById(request.params.fileId);
-        
-        file.downloadCount++;
-
-        await file.save();
-
-        response.download(file.path, file.name);
-    } catch (error) {
-        console.error(error.message);
-        response.status(500).json({ msg: error.message });
-    }
-}
+export const downloadImage = async (req, res) => {
+  try {
+    const file = await File.findById(req.params.fileId);
+    file.downloadCount++;
+    await file.save();
+    res.download(file.path, file.name);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
