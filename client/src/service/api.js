aecbf017eb097e4107f9583ad72070221aa911a5
Suyash Mishra <143060685+SuyashMishr@users.noreply.github.com>
2025-05-16 08:28:02 +0530

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,12 +1,19 @@
+
 import axios from 'axios';
 
-const API_URI = 'http://localhost:8000';
+const API_BASE_URL = 'http://localhost:8000';
 
 export const uploadFile = async (data) => {
-    try {
-        const response = await axios.post(`${API_URI}/upload`, data);
-        return response.data;
-    } catch (error) {
-        console.log('Error while calling the API ', error.message);
-    }
-}
+  try {
+    const response = await axios.post("http://localhost:8000/upload", data, {
+      headers: {
+        "Content-Type": "multipart/form-data"
+      }
+    });
+    console.log("✅ Server response:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error('Error uploading file:', error);
+    return null;
+  }
+};
